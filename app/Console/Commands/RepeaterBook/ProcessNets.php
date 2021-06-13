841dc7aeba66430c02ea54532e2076da9da39348
Jim Keener <jim@jimkeener.com>
2021-06-13 13:49:51 -0400

    Process timezones a little better

--- app/Console/Commands/RepeaterBook/ProcessNets.php
+++ app/Console/Commands/RepeaterBook/ProcessNets.php
@@ -50,6 +50,32 @@ class ProcessNets extends Command
             array_map(fn($x) => str_replace(["\r", "\n"], "", $x),
             array_map('trim',
             preg_split('~<br */?>~', $repeaterWithNets->nets))));
+          $timezones = [
+            'eastern',
+            'eastern time',
+            '\(eastern time, standard or daylight savings\)',
+            'et',
+            'est',
+            'edt',
+
+            'central',
+            'ct',
+            'cst',
+            'cdt',
+
+            'mountain',
+            'mountain time',
+            'mt',
+            'mst',
+            'mdt',
+
+            'pacific',
+            'pacific time',
+            'pt',
+            'pst',
+            'pdt',
+          ];
+          $timezones = join('|', array_map(fn ($x) => "($x)", $timezones));
           $regex = "(?<name>.+)(:|(--?))?" .
             " *((?<every>every)|((the)?(?<first>first)|(?<second>second)|(?<third>third)|(?<fourth>fourth)))? *".
             " *(and ((?<first2>first)|(?<second2>second)|(?<third2>third)|(?<fourth2>fourth)))? *".
@@ -63,7 +89,11 @@ class ProcessNets extends Command
             "(?<saturday>sat(urday)?)".
             ")s?".
             " *((evening)|(night)|(morning)|(of ((the)|(each)) month))? *" .
-            " +((at)|@)? *(?<time>[0-9]+:?[0-9]{2}( *[ap]m)?)(hrs)?( *(except holidays)?(?<timezone>(?<localtime>local time)|([^\.]*)))?\.?";
+            " +((at)|@)? *".
+            "(?<time>[0-9]+:?[0-9]{2}( *([ap]m)|([ap] ))?)( *hrs)?" .
+            "( *- *(?<endtime>[0-9]+:?[0-9]{2}( *([ap]m)|([ap] ))?)( *hrs)?)?" .
+            "( *(except holidays))?".
+            "(?<timezone>(?<localtime>((local time)|(local)|(\(local\))))|{$timezones})?\.?";
           DB::table('net')->where('primary_repeaterbook_state_id', '=', $repeaterWithNets->state)
                           ->where('primary_repeaterbook_repeater_id', '=', $repeaterWithNets->id)
                           ->delete();
@@ -73,16 +103,39 @@ class ProcessNets extends Command
               $matches = array_map('trim', $matches);
               if (!empty($matches['localtime'])) {
                 $matches['timezone'] = $repeaterWithNets->tzid;
+              } elseif(!array_key_exists('timezone', $matches)) {
+                $matches['timezone'] = $repeaterWithNets->tzid;
               } elseif (in_array(strtolower($matches['timezone']), ['eastern time', 'eastern', 'et', '(eastern time, standard or daylight savings)'])) {
                 $matches['timezone'] = 'America/New_York';
               } elseif (in_array(strtolower($matches['timezone']), ['central time', 'central', 'ct'])) {
                 $matches['timezone'] = 'America/Chicago';
               } elseif (in_array(strtolower($matches['timezone']), ['mountain time', 'mountain', 'mt'])) {
                 $matches['timezone'] = 'America/Denver';
-              } elseif (in_array(strtolower($matches['timezone']), ['pacific time', 'pacific', 'pt'])) {
+              } elseif (in_array(strtolower($matches['timezone']), ['pacific time', 'pacific', 'pt', 'pst'])) {
                 $matches['timezone'] = 'America/Los_Angeles';
               } elseif (empty($matches['timezone'])) {
                 $matches['timezone'] = $repeaterWithNets->tzid;
+              } elseif (preg_match('/\d/', $matches['timezone'])) {
+                var_dump($net);
+                var_dump($matches['timezone']);
+                $matches['timezone'] = $repeaterWithNets->tzid;
+              }
+
+              if (preg_match('/(\d{2}):(\d{2}) *[ap]m?/i', $matches['time'], $timematch)) {
+                if (intval($timematch[1]) > 12) {
+                  $matches['time'] = $timematch[1] . ':' .  $timematch[2];
+                }
+              }
+              if (preg_match('/(\d{1,2})(\d{2}) *([ap]m?)/i', $matches['time'], $timematch)) {
+                if (strlen($timematch[3]) == 1) {
+                  $timematch[3] .= 'm';
+                }
+                if (intval($timematch[1]) > 12) {
+                  $timematch[3] = '';
+                } else {
+                  $timematch[3] = ' ' . $timematch[3];
+                }
+                $matches['time'] = $timematch[1] . ':' .  $timematch[2] . $timematch[3];
               }
               $row = [
                 'created_at' => DB::raw('now()'),
