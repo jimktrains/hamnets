0075702244f430141463aabf7f69384d982eb1e7
Jim Keener <jim@jimkeener.com>
2024-12-14 00:43:09 -0500

    fixing backup script

--- crons/backup.sh
+++ crons/backup.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 SQLFILE=hamnets-`date +%Y-%m-%d-%H-%M`.sql
 S3PREFIX=s3://jsk-backup/hamnets/db/`date +%Y/%m/%d`
 
-pg_dump --exclude-table=gadm36 hamnets > $SQLFILE
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  rm $SQLFILE
+}
+
+pg_dump --exclude-table=gadm36 --exclude-table=gadm_410 hamnets > $SQLFILE
 s3cmd put $SQLFILE $S3PREFIX/$SQLFILE
